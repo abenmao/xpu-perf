537d307055d04764b1c11e1237be3f77882fcbc8
nc-BobLee <bo.o.li@intel.com>
2026-04-16 08:52:54 +0000

    remove sycl extention code.

--- micro_perf/backends/INTEL/ops/torch/head_rms_norm.py
+++ micro_perf/backends/INTEL/ops/torch/head_rms_norm.py
@@ -8,24 +8,8 @@ sys.path.insert(
     0,
     str(pathlib.Path(__file__).absolute().parents[4])
 )
-sys.path.insert(0, "/mnt/disk1/libo/bytedance/ops/rmsnorm/")
-
 from core.utils import calc_tensor_size
 from core.ops.llm_ops import HeadRMSNormOp as BaseHeadRMSNormOp
-
-# Optional SYCL extension import
-_sycl_extension_import_error = None
-try:
-    import sycl_extension
-except Exception as exc:
-    _sycl_extension_import_error = exc
-    print(
-        "Failed to import sycl_extension from /mnt/disk1/libo/bytedance/ops/rmsnorm/",
-        file=sys.stderr,
-    )
-    traceback.print_exc()
-    sycl_extension = None
-
 
 class HeadRMSNormTorchOp(BaseHeadRMSNormOp):
 
@@ -64,24 +48,12 @@ class HeadRMSNormTorchOp(BaseHeadRMSNormOp):
             eps=self.eps,
         )
 
-
-    def _head_rms_norm_sycl(self, head_data, norm_weight):
-        if sycl_extension is None:
-            raise RuntimeError(
-                "sycl_extension is not available. Please build/install /mnt/disk1/libo/bytedance/ops/rmsnorm first. "
-                f"Original import error: {_sycl_extension_import_error!r}"
-            )
-        return sycl_extension.rmsnorm_optimize.forward(head_data, norm_weight, self.eps)
-
-
     def vendor_impl_run(self, tensor_mapping):
         token_data = tensor_mapping["token_data"]
         norm_weight = tensor_mapping["norm_weight"]
 
         head_data = token_data[:, self.norm_head_start:self.norm_head_end, :]
-        #normed_data = self._head_rms_norm_sycl(head_data.contiguous(), norm_weight)
         normed_data = self._head_rms_norm_compiled(head_data.contiguous(), norm_weight)
-
         head_data.copy_(normed_data)
         return token_data
 

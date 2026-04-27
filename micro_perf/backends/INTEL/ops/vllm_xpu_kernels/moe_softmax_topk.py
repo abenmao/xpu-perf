01334cd973097f641e2e6bebde79bbd381e7cf9f
root <xiaolong.guo@intel.com>
2026-04-27 01:59:35 +0000

    correct Inherited property
    Signed-off-by: root <xiaolong.guo@intel.com>

--- micro_perf/backends/INTEL/ops/vllm_xpu_kernels/moe_softmax_topk.py
+++ micro_perf/backends/INTEL/ops/vllm_xpu_kernels/moe_softmax_topk.py
@@ -12,6 +12,7 @@ from core.op import ProviderRegistry
 from core.ops.llm_ops import MoeSoftmaxTopkOp
 from core.utils import OpTensorInfo, calc_tensor_size
 
+
 try:
     import vllm_xpu_kernels._moe_C
 
@@ -26,83 +27,33 @@ try:
                 create_inputs=True,
                 create_outputs=True,
             )
-        
-        def prepare(self):
-            self.arg_type = self.args_dict["arg_type"]
-            if not self.arg_type in ["llm"]:
-                raise NotImplementedError
+            self.output_tensor_info["token_expert_indices"] = OpTensorInfo(
+                shape=[self.num_tokens * self.topk],
+                dtype=torch.int32,
+                device=self.backend.get_torch_device_name(),
+            )
 
-            self.dtype = self.args_dict["dtype"]
-            if not self.dtype in ["float32"]:
-                raise NotImplementedError
-            self.torch_dtype = getattr(torch, self.dtype)
-
-            self.num_experts = self.args_dict["num_experts"]
-            
-            self.topk = self.args_dict["topk"]
-
-            self.compute_mode = self.args_dict["compute_mode"]
-            if not self.compute_mode in ["pre-softmax", "post-softmax"]:
-                raise NotImplementedError
-
-            self.sp_size = self.args_dict.get("sp_size", 1)
-            self.num_tokens = self.args_dict["num_tokens"] // self.sp_size
-            self.hidden_size = self.args_dict["hidden_size"]
-
-            self.input_tensor_info = {
-                "gating_output": OpTensorInfo(
-                    shape=[self.num_tokens, self.num_experts],
-                    dtype=self.torch_dtype,
-                    device=self.backend.get_torch_device_name(),
-                )
-            }
-            self.output_tensor_info = {
-                "selected_experts": OpTensorInfo(
-                    shape=[self.num_tokens, self.topk],
-                    dtype=torch.int32,
-                    device=self.backend.get_torch_device_name(),
-                ),
-                "moe_weights": OpTensorInfo(
-                    shape=[self.num_tokens, self.topk],
-                    dtype=self.torch_dtype,
-                    device=self.backend.get_torch_device_name(),
-                ),
-                "token_expert_indices": OpTensorInfo(
-                    shape=[self.num_tokens * self.topk],
-                    dtype=torch.int32,
-                    device=self.backend.get_torch_device_name(),
-                )
-            }
-
-            self.input_tensor_size = sum([
-                calc_tensor_size(info) for info in self.input_tensor_info.values()
-            ])
-            self.output_tensor_size = sum([
+            self.output_tensor_size = sum(
                 calc_tensor_size(info) for info in self.output_tensor_info.values()
-            ])
+            )
             self.tensor_size = self.input_tensor_size + self.output_tensor_size
-
-            self.read_bytes = self.input_tensor_size
             self.write_bytes = self.output_tensor_size
-            self.io_bytes = self.read_bytes + self.write_bytes
-
-            self.algo_size = 0
-            self.bus_size = 0
-
-            self.calc_flops = self.num_tokens * (5 * self.num_experts + self.topk * self.num_experts)
-            self._run_func = self.vendor_impl_run
-
+            self.io_bytes = self.read_bytes + self.write_bytes   
+        
         def vendor_impl_run(self, tensor_mapping):
             gating_output = tensor_mapping["gating_output"]
             selected_experts = tensor_mapping["selected_experts"]
             moe_weights = tensor_mapping["moe_weights"]
             token_expert_indices = tensor_mapping["token_expert_indices"]
+            # _moe_C::topk_softmax needs int32 output for indices
             
+            renormalize = self.compute_mode == "pre-softmax"
             torch.ops._moe_C.topk_softmax(
                 moe_weights, selected_experts, token_expert_indices,
-                gating_output, True , None
+                gating_output, renormalize, None
             )
 
+            # copy back int32 indices to the pre-allocated float32 tensor
             return selected_experts, moe_weights
 
 except Exception:
